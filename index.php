9b0f75e8ff524eca47ba3f8ca0ee15cfe41b150d
indefpro <indefpro@gmail.com>
2016-07-05 16:58:28 +0300

    Добавился класс - hello

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+class hello{
+	var $hello;
+	var $world;
+	
+	function say(){
+		echo $hello. ' ' .$world;
+	}
+}
+$sayhi = new hello;
+
+$sayhi->$hello = "Привет";
+$sayhi->$world = "Мир";
+
+$sayhi->say();
+
+?>
